533d9d953a049ca441e683296e9a04dc7a957a4a
oussamab <oussama@MacBook-Pro-de-oussamab.local>
2023-02-15 10:37:18 +0100

    commit

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -8,7 +8,7 @@ const Payment = () => {
   const { title } = location.state;
   const { price } = location.state;
   const stripePromise = loadStripe(
-    "pk_test_51IpvphDqQKb3lCIT3UU1fIPnAXyyG57gLns831kNwLVGCFo1a3MtSucuiIwEijgip8fL85zUlKZKTK0a2JAhSWHt00ZWSjTErF"
+    "pk_test_51HCObyDVswqktOkX6VVcoA7V2sjOJCUB4FBt3EOiAdSz5vWudpWxwcSY8z2feWXBq6lwMgAb5IVZZ1p84ntLq03H00LDVc2RwP"
   );
 
   return (
